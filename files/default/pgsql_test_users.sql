1afef2beae55aee5db654b39dec6eb8136225157
Said Sef <said.sef@telegraph.co.uk>
2015-10-21 00:26:25 +0100

    pgsql db sql fix

--- files/default/pgsql_test_users.sql
+++ files/default/pgsql_test_users.sql
@@ -1,4 +1,4 @@
-DROP DATABAE test_users;
-CREATE DATABAE test_users;
+DROP DATABASE IF EXISTS test_users;
+CREATE DATABASE test_users;
 \c test_users
 CREATE TABLE users ( id serial primary key, first_name text, last_name text, age int);
